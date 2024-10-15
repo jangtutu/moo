d1bdbb73a4265d349c8f518adcf0b7e9b56d1a89
장원 <jangwon_s@naver.com>
2024-10-15 23:02:25 +0900

    홈화면

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['liveimg.sooplive.co.kr'],
+    },
+  };
+  
+  export default nextConfig;
